1dd328d30ebb5f17f2fa72d7419128ff60344879
AleksandrZaec <zaecam95@mail.ru>
2023-10-20 14:12:12 +0300

    /

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'Name: {name}, (Email: {email}), Message: {message}')
+    return render(request, 'main/contact.html')
